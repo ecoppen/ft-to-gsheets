9efe6b4739aa55116be385688d6d6433f4a05e97
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-09-15 17:08:48 +0100

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -4,9 +4,10 @@ from pathlib import Path
 
 import pygsheets
 import sqlalchemy as db
+from sqlalchemy import exc
 
 secrets_file = Path("client_secret.json")
-freqtrade_database = Path(Path.home(), "freqtrade", "user_data", "tradesv3.sqlite")
+freqtrade_database = Path(Path.home(), "freqtrade", "tradesv3.sqlite")
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -30,11 +31,26 @@ def check_authorisation(file):
     return False
 
 
+def check_database_connection(file):
+    try:
+        engine = db.create_engine(f"sqlite:///{file}.db?check_same_thread=false")
+        engine.table_names()
+        return True
+    except exc.OperationalError as e:
+        log.error(f"Loading {file} has failed: {e}")
+    return False
+
+
 def initial_checks(google_file, freqtrade_file):
     if check_file_exists(google_file):
         if check_authorisation(google_file):
             if check_file_exists(freqtrade_file):
-                return True
+                if check_database_connection(freqtrade_file):
+                    return True
+                else:
+                    log.error(
+                        f"This isn't a comptaible database file: {freqtrade_file}"
+                    )
             else:
                 log.error(
                     f"Freqtrade database does not exist at this location, please check path: {freqtrade_database}"
@@ -53,6 +69,10 @@ def initial_checks(google_file, freqtrade_file):
 def main():
     if initial_checks(google_file=secrets_file, freqtrade_file=freqtrade_database):
         client = pygsheets.authorize(service_file=secrets_file)
+        engine = db.create_engine(
+            f"sqlite:///{freqtrade_database}.db?check_same_thread=false"
+        )
+        print(engine.table_names())
         sh = client.open("Cryptobot")
         worksheet = sh.worksheet("title", "bi-usdt-trades")
         sh.del_worksheet(worksheet)
