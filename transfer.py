bc01aba65cd94b6ca03d737ab18e580760fa8614
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-09-21 21:10:30 +0100

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -35,15 +35,56 @@ def check_authorisation(file):
     return False
 
 
-def check_workbook_sheet_exists(file):
+def setup_new_workbook(service_account, workbook_name, sheet_name):
+    sh = service_account.create(workbook_name)
+    log.info(f"Created workbook - {workbook_name}")
+    worksheet = sh.sheet1
+    worksheet.update_title("Summary")
+    log.info("Added worksheet - Summary")
+    sh.add_worksheet(title=sheet_name, rows=1000, cols=30)
+    log.info(f"Added worksheet - {sheet_name}")
+    sh.add_worksheet(title="data processing", rows=1000, cols=30)
+    log.info("Added worksheet - data processing")
+
+
+def setup_new_worksheet(service_account, workbook_name, sheet_name):
+    sh = service_account.create(workbook_name)
+    worksheet_list = sh.worksheets()
+    sheets_to_create = ["Summary", sheet_name, "data processing"]
+    for sheet in sheets_to_create:
+        if sheet not in worksheet_list:
+            sh.add_worksheet(title=sheet_name, rows=1000, cols=30)
+            log.info(f"Added worksheet - {sheet_name}")
+    if "Sheet1" in worksheet_list:
+        sh.del_worksheet("Sheet1")
+        log.info("Deleted worksheet - Sheet1")
+
+
+def check_workbook_sheet_exists(file, recheck_workbook=False, recheck_worksheet=False):
     try:
         gc = gspread.service_account(filename=str(file))
         gc.open(google_workbook_name).worksheet(google_workbook_sheet_name)
         return True
     except gspread.exceptions.SpreadsheetNotFound:
         log.error(f"This workbook does not exist: {google_workbook_name}")
+        if not recheck_workbook:
+            log.info(f"Attempting to setup: {google_workbook_name}")
+            setup_new_workbook(
+                service_account=gc,
+                workbook_name=google_workbook_name,
+                worksheet_name=google_workbook_sheet_name,
+            )
+            return check_workbook_sheet_exists(file=file, recheck_workbook=True)
     except gspread.exceptions.WorksheetNotFound:
         log.error(f"This worksheet does not exist: {google_workbook_sheet_name}")
+        if not recheck_worksheet:
+            log.info(f"Attempting to setup: {google_workbook_sheet_name}")
+            setup_new_worksheet(
+                service_account=gc,
+                workbook_name=google_workbook_name,
+                worksheet_name=google_workbook_sheet_name,
+            )
+            return check_workbook_sheet_exists(file=file, recheck_worksheet=True)
     return False
 
 
@@ -64,22 +105,21 @@ def check_database_connection(file):
 def initial_checks(google_file, freqtrade_file):
     if check_file_exists(google_file):
         if check_authorisation(google_file):
-            if (
-                google_workbook_name != ""
-                and google_workbook_sheet_name != ""
-                and check_workbook_sheet_exists(google_file)
-            ):
-                if check_file_exists(freqtrade_file):
-                    if check_database_connection(freqtrade_file):
-                        return True
+            if google_workbook_name != "" and google_workbook_sheet_name != "":
+                if check_workbook_sheet_exists(google_file):
+                    if check_file_exists(freqtrade_file):
+                        if check_database_connection(freqtrade_file):
+                            return True
+                        else:
+                            log.error(
+                                f"This isn't a compatible database file: {freqtrade_file}"
+                            )
                     else:
                         log.error(
-                            f"This isn't a compatible database file: {freqtrade_file}"
+                            f"Freqtrade database does not exist at this location, please check path: {freqtrade_database}"
                         )
                 else:
-                    log.error(
-                        f"Freqtrade database does not exist at this location, please check path: {freqtrade_database}"
-                    )
+                    log.error("Error finding or setting up Google Sheets")
             else:
                 log.error("The Google Sheet has not been setup or referenced correctly")
         else:
